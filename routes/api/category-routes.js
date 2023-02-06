dc5d910ac2e6a6297f788409a6cfce6311a0fbaa
SamsonKnightz <113211927+SamsonKnightz@users.noreply.github.com>
2023-02-06 01:03:00 -0500

    finally got get 1 category by id to work, used routes from 26-stu_literals

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -14,15 +14,29 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', async (req, res) => {
+
   // find one category by its `id` value
   // be sure to include its associated Products
-
-    const categoryData = await Category.findByPk(req.params.id,
-      include [Product]);
-      res.status(200).json(categoryData);
-
-});
+  
+    // const categoryData = await Category.findByPk(req.params.id,
+    //   include [Product]);
+    //   res.status(200).json(categoryData);
+    router.get('/:id', async (req, res) => {
+      try {
+        const categoryData = await Category.findByPk(req.params.id, {
+          include: [{ model: Product }],
+        });
+    
+        if (!categoryData) {
+          res.status(404).json({ message: 'No category found with that id!' });
+          return;
+        }
+    
+        res.status(200).json(categoryData);
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    });
 
 
 // Create new category POST function at api/categories/
