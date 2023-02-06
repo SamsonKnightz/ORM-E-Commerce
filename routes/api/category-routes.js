d60b6efd45a943e19f7eadcec0eb70f901560f02
SamsonKnightz <113211927+SamsonKnightz@users.noreply.github.com>
2023-02-06 00:16:22 -0500

    sync sequelize to the database on server start

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,29 +1,38 @@
 const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
-// The `/api/categories` endpoint
-
+// Get all route for /api/categories/
+// Try setting finding all categorydata in json including product, send data or catch error response
 router.get('/', async (req, res) => {
   try {
-    const categoryData = await Category.findAll(
-      include [Product]);
-      res.status(200).json(categoryData);
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  });
-
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+    const categoryData = await Category.findAll({
+      include: [{ model: Product }],
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
+router.get('/:id', async (req, res) => {
+  // find one category by its `id` value
+  // be sure to include its associated Products
+
+    const categoryData = await Category.findByPk(req.params.id,
+      include [Product]);
+      res.status(200).json(categoryData);
+
+});
+
+
+// Create new category POST function at api/categories/
+// Function will try to create a new category, only when request is made to create, we will then send over the newly created category date over as a response.
 router.post('/', async (req, res) => {
-  // create a new category
   try {
-    const categoryData = await Category.create(req.body)
-    .then(categoryData => res.json(categoryData))
-    res.status(200).json(categoryData);
+    const categoryData = await Category.create({
+      category_name: req.body.category_name,
+      });
+      res.status(200).json(categoryData);
   } catch (err) {
     res.status(500).json(err);
   }
