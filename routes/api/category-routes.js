1f68535ed6347824f3872ffd244c3eeab54a4254
SamsonKnightz <113211927+SamsonKnightz@users.noreply.github.com>
2023-02-06 01:09:47 -0500

    was able to remove category by id routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -14,13 +14,6 @@ router.get('/', async (req, res) => {
   }
 });
 
-
-  // find one category by its `id` value
-  // be sure to include its associated Products
-  
-    // const categoryData = await Category.findByPk(req.params.id,
-    //   include [Product]);
-    //   res.status(200).json(categoryData);
     router.get('/:id', async (req, res) => {
       try {
         const categoryData = await Category.findByPk(req.params.id, {
@@ -52,12 +45,23 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
-});
+router.delete('/:id', async (req, res) => {
+  try {
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+    if (!categoryData) {
+      res.status(404).json({ message: 'No library card found with that id!' });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
