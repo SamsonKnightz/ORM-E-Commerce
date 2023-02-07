d25e9235381113e3af54750e5b2134cea86144ba
SamsonKnightz <113211927+SamsonKnightz@users.noreply.github.com>
2023-02-06 20:53:15 -0500

    got most routes working for tag and products. update tag route still not updating

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -5,8 +5,6 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // get all products
 router.get('/', async (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
   try {
   const productData = await Product.findAll({
     attributes: ["id", "product_name", "price", "stock", "category_id"],
@@ -21,9 +19,17 @@ router.get('/', async (req, res) => {
 });
 
 // get one product
-router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+router.get('/:id', async(req, res) => {
+  try {
+    const productData = await Product.findByPk(req.params.id);
+    if (!productData) {
+      res.status(404).json({ message: 'No Tag found with that id!' });
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
@@ -100,8 +106,19 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    const productData = await Product.destroy({
+      where: {id: req.params.id,}
+    });
+    if (!productData) {
+      res.status(404).json({ message: 'No library card found with that id!' });
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -32,7 +32,7 @@ router.get('/:id', async (req, res) => {
 router.post('/', async (req, res) => {
   try {
     const tagData = await Tag.create({
-      tag_name: req.body.tag_name,
+      where : {tag_name: req.body.tag_name,}
     });
     res.status(200).json(tagData);
   } catch (err) {
@@ -40,12 +40,36 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+router.put('/:id', async (req, res) => {
+  const tagData = await Tag.update(req.body, {
+    where: { id: req.params.id }
+  })
+    .then(tagData => {
+      if (!tagData) {
+        res.status(404).json({ message: "Sorry, no tag with this id was found!" });
+        return;
+      }
+      res.json({ message: "This product was successfully updated!" });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
-router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    const tagData = await Tag.destroy({
+      where: {id: req.params.id,}
+    });
+    if (!tagData) {
+      res.status(404).json({ message: 'No library card found with that id!' });
+      return;
+    }
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
